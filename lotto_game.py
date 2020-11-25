a1fbad6c3ea036ec13afe59ee5409a48a4c5bab1
Amanikashema <61458926+Amanikashema@users.noreply.github.com>
2020-11-25 16:35:11 +0200

    Add files via upload
    second day work update

--- lotto_game.py
+++ lotto_game.py
@@ -3,7 +3,6 @@ from tkinter import *
 import random
 from tkinter import messagebox as mb
 import datetime as dt
-
 root = Tk()
 root.title("Validation")
 root.geometry("500x500")
@@ -12,12 +11,12 @@ root.configure(bg="green")
 
 class Lotto:
     def __init__(self):
-
+        # function to validate login
         def login():
             try:
                 if int(self.age.entry.get()) < 18:
                     mb.showwarning("Sorry","You do not qualify to play lotto")
-            finally:
+
                 if int(self.age.entry.get()) >= 18:
                     mb.showinfo("You qualify to play Lotto","Welcome to the Ithuba National Lottery of South Africa")
                     root.destroy()
@@ -35,72 +34,88 @@ class Lotto:
                         input_list.append(int(a6.get()))
 
                         lotto_numbers = sorted(random.sample(range(1,49),6))
+                        filename = "text_file.txt"
 
+                        # statement for decision
                         if len(lotto_numbers) == len(input_list):
                             duplicates = set(lotto_numbers).intersection(set(input_list))
                             if len(duplicates) == 0:
-                                answer.configure(text="You failed And the" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R0")
+                                answer.configure(text="You failed !!! " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R0 \n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 1:
-                                answer.configure(text="Congratulations You got 1 number correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R0")
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 1 number correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R0\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 2:
-                                answer.configure(text="Congratulations You got 2 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R20" )
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 2 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R20\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 3:
-                                answer.configure(text="Congratulations You got 3 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R110" )
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 3 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R110\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 4:
-                                answer.configure(text="Congratulations You got 4 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R2384" )
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 4 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R2384\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 5:
-                                answer.configure(text="Congratulations You got 5 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R8584" )
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 5 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrize: R8584\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
                             elif len(duplicates) == 6:
-                                answer.configure(text="Congratulations You got 6 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrice: R10 000 000" )
+                                answer.configure(text="Congratulations " + str(self.var1.get()) + ' ' + str(self.var2.get()) + "\n You got 6 numbers correct \n" + "Lucky Lotto Numbers are: " + str(lotto_numbers) + "\nPrice: R10 000 000\n" + "Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
+                            file = open(filename, "w+")
+                            file.close()
+                            file = open(filename, "a")
+                            answers = answer.cget("text")
+                            file.write(answers)
+                            file.close()
 
                     # lotto numbers
-                    answer = Label(root1)
-                    answer.place(x=40,y=200)
+                    answer = Label(root1,bg="white")
+                    answer.place(x=100,y=200)
                     input_list = []
 
+                    label_text = Label(root1, text="Enter lucky Lotto Numbers")
+                    label_text.place(x=10,y=20)
+
+                    # lotto entries
                     a1 = Entry(root1, width=4)
-                    a1.place(x=0,y=10)
+                    a1.place(x=200,y=20)
 
                     a2 = Entry(root1, width=4)
-                    a2.place(x=50,y=10)
+                    a2.place(x=250,y=20)
 
                     a3 = Entry(root1, width=4)
-                    a3.place(x=100,y=10)
+                    a3.place(x=300,y=20)
 
                     a4 = Entry(root1, width=4)
-                    a4.place(x=150,y=10)
+                    a4.place(x=350,y=20)
 
                     a5 = Entry(root1, width=4)
-                    a5.place(x=200,y=10)
+                    a5.place(x=400,y=20)
 
                     a6 = Entry(root1, width=4)
-                    a6.place(x=250,y=10)
+                    a6.place(x=450,y=20)
 
+                    # Button to submit both list
                     button = Button(root1,text="submit",command=random_lotto)
-                    button.place(x=10,y=60)
-
-
+                    button.place(x=250,y=130)
+            finally:
+                pass
         # label
         self.user_name = Label(root,text="Please enter your name")
         self.user_surname = Label(root,text="Please enter your Surname")
         self.age = Label(root,text="PLease enter your age")
-        self.submit = Button(root,text="Check validation",command=login).place(x=50, y=100)
+        self.submit = Button(root,text="Check validation",command=login).place(x=170, y=230)
+
         # time label
         now = dt.datetime.now()
         self.time = Label(root)
-        self.time.place(x=50, y=150)
+        self.time.place(x=150, y=280)
         self.time.config(text="Date: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
 
         # entry
-        self.user_name_entry = Entry(root).place(x=190, y=10)
-        self.user_surname_entry = Entry(root).place(x=190, y=40)
+        self.var1 = StringVar()
+        self.var2 = StringVar()
+        self.user_name_entry = Entry(root,textvariable=self.var1).place(x=290, y=50)
+        self.user_surname_entry = Entry(root,textvariable=self.var2).place(x=290, y=100)
         self.age.entry = Entry(root)
 
         # positioning
-        self.user_name.place(x=0, y=10)
-        self.user_surname.place(x=0, y=40)
-        self.age.place(x=0, y=70)
-        self.age.entry.place(x=190, y=70)
+        self.user_name.place(x=50, y=50)
+        self.user_surname.place(x=50, y=100)
+        self.age.place(x=50, y=150)
+        self.age.entry.place(x=290, y=150)
 
 
 Lotto()
